3862b530eae9d36298c025016a9a4b5efcf1e9a3
Alexander Tegelid <alex@tegelid.se>
2015-09-24 15:03:04 +0200

    add default message and search form to the 404

--- 404.php
+++ 404.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
 
+  <div class="page-body">
+
+    <p><?php _e( 'Sorry, no posts matched your criteria.', 'hmbl' ); ?></p>
+    <p><?php get_search_form(); ?></p>
+
+  </div>
+
 <?php get_footer(); ?>
