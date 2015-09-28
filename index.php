5a2b35c3cecbf6777399b7271a57836739e545bc
Alexander Tegelid <alex@tegelid.se>
2015-09-28 14:12:29 +0200

    add post loop

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php get_header(); ?>
 
+  <div class="wrapper page-wrapper">
+
+    <?php if (have_posts()): while(have_posts()): the_post(); ?>
+
+      <div class="wrapper content-wrapper">
+        <h1><?php the_title(); ?></h1>
+        <div>
+          <?php the_content(); ?>
+        </div>
+      </div>
+
+    <?php endwhile; else: ?>
+      <p><?php _e( 'Sorry, no posts matched your criteria.', 'hmbl' ); ?></p>
+    <?php endif; ?>
+
+  </div>
+
 <?php get_footer(); ?>
