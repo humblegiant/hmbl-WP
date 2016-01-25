e7e90c4c9845da09eea4208f0e91b0daefa618ef
Alexander Tegelid <alex@tegelid.se>
2016-01-25 15:12:39 +0100

    add title tag

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<title><?php echo is_front_page() ? get_bloginfo('description').' » ' : wp_title( '»', true, 'right' ); ?><?php bloginfo('name'); ?></title>
 	<?php wp_head(); ?>
 </head>
 
